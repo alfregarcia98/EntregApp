dab1bf07c087da5caefb026fb293d41e15557bcd
alfgarlla <alfregarciallanera98@gmail.com>
2023-04-29 01:52:18 +0200

    Las estadisticas se suben en Json a firestore. Mejorar nombre StatsService a uno mas correcto

--- app/src/main/java/com/dam/entregapp/ui/fragments/Statistics/Statistics.kt
+++ app/src/main/java/com/dam/entregapp/ui/fragments/Statistics/Statistics.kt
@@ -1,16 +1,17 @@
 package com.dam.entregapp.ui.fragments.Statistics
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.dam.entregapp.ChartActivity
 import com.dam.entregapp.LocationApp.Companion.prefs
 import com.dam.entregapp.R
 import com.dam.entregapp.databinding.FragmentStatisticsBinding
+import com.dam.entregapp.service.StatisticsService
 import com.dam.entregapp.ui.viewmodels.UserViewModel
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.ktx.firestore
@@ -23,10 +24,8 @@ import java.util.*
 
 class Statistics : Fragment(R.layout.fragment_statistics) {
 
+    private lateinit var statisticsService: StatisticsService
     private lateinit var userViewModel: UserViewModel
-
-    var porcentajePrincipal = 0f
-    var porcentajeSecundario = 0f
 
     private var _binding: FragmentStatisticsBinding? = null
     private val binding get() = _binding!!
@@ -50,6 +49,10 @@ class Statistics : Fragment(R.layout.fragment_statistics) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        statisticsService = StatisticsService(userViewModel)
+
+        binding.addr1Name.text = prefs.getPrimaryAddressName()
+        binding.addr2Name.text = prefs.getSecondaryAddressName()
 
         binding.btnBorrar.setOnClickListener {
             borrarTracking()
@@ -59,33 +62,62 @@ class Statistics : Fragment(R.layout.fragment_statistics) {
             submitStatistics()
         }
 
-
-        //Para evitar el error de cambiar la ui desde corrutina
-        /*fun Fragment?.runOnUiThread(action: () -> Unit) {
-            this ?: return
-            if (!isAdded) return // Fragment not attached to an Activity
-            activity?.runOnUiThread(action)
-
-            binding.btnBorrar.setOnClickListener {
-                borrarTracking()
-            }
-
-            binding.btnEnviar.setOnClickListener {
-                submitStatistics()
-            }
-
-            //TODO Query para agrupar por horas las ubicaciones
-            //SELECT count(id), strftime('%H',date/1000, 'unixepoch','localtime') as hour from tracking_data_table group by hour;
-            //TODO Query para agrupar por horas y por address_id las ubicaciones
-            //SELECT address_id,  strftime('%H',date/1000, 'unixepoch','localtime') as hour, count(id) from tracking_data_table group by hour,address_id order by hour;
-
-            //SELECT address_id,  strftime('%H',date/1000, 'unixepoch','localtime') as hour, count(id) as data_count from tracking_data_table group by hour,address_id order by hour;
-
+        binding.btnChart.setOnClickListener {
+            val intent = Intent(activity, ChartActivity::class.java)
+            startActivity(intent)
         }
+    }
 
-            binding.addr1Name.text = prefs.getPrimaryAddressName()
-            binding.addr2Name.text = prefs.getSecondaryAddressName()
-            *//*        CoroutineScope(Dispatchers.IO).launch {
+
+    private fun submitStatistics() = CoroutineScope(Dispatchers.IO).launch {
+
+        val statistics = statisticsService.getProcessedStatistics()
+        val confirmacion = hashMapOf(
+            "mainAddress" to prefs.getPrimaryAddressName(),
+            "secondaryAddress" to prefs.getSecondaryAddressName(),
+            "JSONStats" to statistics.toString(),
+            "fecha" to FieldValue.serverTimestamp()
+        )
+        val db = Firebase.firestore
+        val docRef = db.collection("users").document(prefs.getAuthID())
+
+        docRef.collection("Statistics").document("Tracking").set(confirmacion)
+    }
+
+    private fun borrarTracking() {
+        CoroutineScope(Dispatchers.IO).launch {
+            userViewModel.deleteAllTrackingData()
+        }
+    }
+}
+
+
+//Para evitar el error de cambiar la ui desde corrutina
+/*fun Fragment?.runOnUiThread(action: () -> Unit) {
+    this ?: return
+    if (!isAdded) return // Fragment not attached to an Activity
+    activity?.runOnUiThread(action)
+
+    binding.btnBorrar.setOnClickListener {
+        borrarTracking()
+    }
+
+    binding.btnEnviar.setOnClickListener {
+        submitStatistics()
+    }
+
+    //TODO Query para agrupar por horas las ubicaciones
+    //SELECT count(id), strftime('%H',date/1000, 'unixepoch','localtime') as hour from tracking_data_table group by hour;
+    //TODO Query para agrupar por horas y por address_id las ubicaciones
+    //SELECT address_id,  strftime('%H',date/1000, 'unixepoch','localtime') as hour, count(id) from tracking_data_table group by hour,address_id order by hour;
+
+    //SELECT address_id,  strftime('%H',date/1000, 'unixepoch','localtime') as hour, count(id) as data_count from tracking_data_table group by hour,address_id order by hour;
+
+}
+
+    binding.addr1Name.text = prefs.getPrimaryAddressName()
+    binding.addr2Name.text = prefs.getSecondaryAddressName()
+    *//*        CoroutineScope(Dispatchers.IO).launch {
 
                     val trackingData = userViewModel.getTrackingData()
 
@@ -116,24 +148,3 @@ class Statistics : Fragment(R.layout.fragment_statistics) {
             binding.porcentaje1Txt.text = porcentajePrincipal.toString()
             binding.porcentaje2Txt.text = porcentajeSecundario.toString()
         }*/
-    }
-
-
-    private fun submitStatistics() {
-
-        val confirmacion = hashMapOf(
-            "disponibilidad" to "Si",
-            "fecha" to FieldValue.serverTimestamp()
-        )
-        val db = Firebase.firestore
-        val docRef = db.collection("users").document(prefs.getAuthID())
-
-        docRef.collection("Statistics").document("Tracking").set(confirmacion)
-    }
-
-    private fun borrarTracking() {
-        CoroutineScope(Dispatchers.IO).launch {
-            userViewModel.deleteAllTrackingData()
-        }
-    }
-}
